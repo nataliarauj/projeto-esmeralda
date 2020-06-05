15b16f21fcb79c92f6d60311b959ba3fd5f11b81
Sardemberg <60553428+Sardemberg@users.noreply.github.com>
2020-06-05 10:49:34 -0300

    Add files via upload

--- projeto-esmeralda/esmeralda.sql
+++ projeto-esmeralda/esmeralda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05-Jun-2020 às 02:17
+-- Tempo de geração: 05-Jun-2020 às 15:47
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.1
 
@@ -25,6 +25,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `atualizacoes`
+--
+
+CREATE TABLE `atualizacoes` (
+  `id` int(10) NOT NULL,
+  `data_hora` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `atualizacoes`
+--
+
+INSERT INTO `atualizacoes` (`id`, `data_hora`) VALUES
+(1, '05/06/2020 15:15'),
+(2, '05/06/2020 15:36');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `soros`
 --
 
@@ -42,13 +61,24 @@ CREATE TABLE `soros` (
 
 INSERT INTO `soros` (`id`, `nome_hospital`, `soro`, `quantidade`, `geolocalizacao`) VALUES
 (1, 'teste', 'Soro antibotrópico', 2, 'teste'),
-(4, 'Teste2', 'Soro anticrotálico', 4, 'teste'),
+(4, 'Teste2', 'Soro anticrotálico', 10, 'teste'),
 (5, 'teste3', 'Soro anticrotálico', 5, 'tt'),
-(6, 'teste', 'Soro antilonomia', 6, 'teste');
+(6, 'teste', 'Soro antilonomia', 6, 'teste'),
+(7, 'São Raimundo', 'Soro antibotrópico-laquético', 7, ''),
+(8, 'São Raimundo', 'Soro antibotrópico-laquético', 7, ''),
+(9, 'São Raimundo', 'Soro antilaquético', 7, 'Testando123'),
+(10, 'TesteHora', 'Soro antilaquético', 2, 'TesteHora'),
+(11, 'Tesnate', 'Soro antielapídico', 3, 'Testando123');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `atualizacoes`
+--
+ALTER TABLE `atualizacoes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `soros`
@@ -61,10 +91,16 @@ ALTER TABLE `soros`
 --
 
 --
+-- AUTO_INCREMENT de tabela `atualizacoes`
+--
+ALTER TABLE `atualizacoes`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de tabela `soros`
 --
 ALTER TABLE `soros`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
