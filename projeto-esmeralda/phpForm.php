5c6a3a24ea76a7eb3a883188b105a7033593b45e
sardemberg <lucassardem@gmail.com>
2021-03-02 17:06:45 -0300

    Atualização de Segurança

--- projeto-esmeralda/phpForm.php
+++ projeto-esmeralda/phpForm.php
@@ -6,31 +6,36 @@
     $quantidade = $_POST['quantidade'];
     $geolocalizacao = $_POST['geolocalizacao'];
     $agora = date('d/m/Y H:i');
-    $querySelecao = "select* from soros where nome_hospital like '%$nome_hospital%' and soro = '$soro'";
+    $querySelecao = "select* from soros where nome_hospital like ? and soro = ?";
+
+    $resul1;
     
-    $resul1 = mysqli_query($conexao, $querySelecao) or die("Erro na pesquisa do banco"); 
-    
-    if($valores = mysqli_fetch_array($resul1)){
-        $queryUpdate = "UPDATE `soros` SET `quantidade`= $quantidade WHERE nome_hospital like "."'%".$valores['nome_hospital']."%'";
+    $stmt1 = mysqli_prepare($conexao, $querySelecao);
+    mysqli_stmt_bind_param($stmt1, "ss", $nome_hospital, $soro);
+    mysqli_stmt_execute($stmt1);
+
+    if(mysqli_stmt_fetch($stmt1)){
+        mysqli_stmt_close($stmt1);
+        $queryUpdate = "update soros set quantidade = ? where nome_hospital like ?";
         
-        $resul2 = mysqli_query($conexao, $queryUpdate) or die("Erro na atualização");
-        
+        $stmt2 = mysqli_prepare($conexao, $queryUpdate);
+        mysqli_stmt_bind_param($stmt2, "is", intval($quantidade), $nome_hospital);
+        mysqli_stmt_execute($stmt2);
         
         $queryInsercaoHora = "insert into atualizacoes(data_hora) values('$agora')";
         echo($queryInsercaoHora);
         $resulHora = mysqli_query($conexao,$queryInsercaoHora) or die("Erro na inserção do horário");
         echo("<script type='text/javascript' language='javascript'>alert('Atualização realidada com sucesso, Obrigado por fortalecer nossa causa!'); window.location.href='formulario.html'</script>");
     }else{
-        $queryInsercao = "INSERT INTO `soros`(`nome_hospital`, `soro`, `quantidade`, `geolocalizacao`) VALUES ('$nome_hospital','$soro',$quantidade, '$geolocalizacao')";
-        
-        $resul = mysqli_query($conexao, $queryInsercao) or die('Erro na inserção do sistema');
+        mysqli_stmt_close($stmt1);
+        $queryInsercao = "insert into soros(nome_hospital, soro, quantidade, geolocalizacao) values (?, ?, ?, ?)";
+
+        $stmt3 = mysqli_prepare($conexao, $queryInsercao);
+        mysqli_stmt_bind_param($stmt3, "ssis", $nome_hospital, $soro, intval($quantidade), $geolocalizacao);
+        mysqli_stmt_execute($stmt3);
 
         $queryInsercaoHora = "insert into atualizacoes(data_hora) values('$agora')";
         $resulHora = mysqli_query($conexao,$queryInsercaoHora) or die("Erro na inserção do horário");
-        echo("<script type='text/javascript' language='javascript'>alert('cadastro realidado com sucesso, Obrigado por fortalecer nossa causa!'); window.location.href='formulario.html'</script>");
+        //echo("<script type='text/javascript' language='javascript'>alert('cadastro realidado com sucesso, Obrigado por fortalecer nossa causa!'); window.location.href='formulario.html'</script>");
     }
-    
-
-    
-    
 ?>
